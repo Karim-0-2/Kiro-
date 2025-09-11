9ecde7fd8804ef51b5da9c4bbaa7e78f8937134f
hasib-1kairim <hasibhassain027@gmail.com>
2025-09-11 12:14:11 +0600

    Update slap.js

--- scripts/cmds/slap.js
+++ scripts/cmds/slap.js
@@ -6,7 +6,7 @@ const OWNER_ID = "61557991443492"; // <-- your owner ID
 module.exports = {
 	config: {
 		name: "slap",
-		version: "1.1",
+		version: "1.2",
 		author: "NTKhang",
 		countDown: 5,
 		role: 0,
@@ -14,26 +14,31 @@ module.exports = {
 		longDescription: "Batslap image",
 		category: "image",
 		guide: {
-			en: "   {pn} @tag"
+			en: "   {pn} @tag or reply to a message"
 		}
 	},
 
 	langs: {
 		vi: {
-			noTag: "Bạn phải tag người bạn muốn tát",
+			noTarget: "Bạn phải tag người bạn muốn tát hoặc reply vào tin nhắn của họ",
 			cantSlapOwner: "Bạn không thể tát chủ bot 😎"
 		},
 		en: {
-			noTag: "You must tag the person you want to slap",
+			noTarget: "You must tag the person you want to slap or reply to their message",
 			cantSlapOwner: "You cannot slap the bot owner 😎"
 		}
 	},
 
 	onStart: async function ({ event, message, usersData, args, getLang }) {
 		const uid1 = event.senderID;
-		const uid2 = Object.keys(event.mentions)[0];
+		let uid2 = Object.keys(event.mentions)[0];
 
-		if (!uid2) return message.reply(getLang("noTag"));
+		// If no mention, check if this is a reply
+		if (!uid2 && event.messageReply) {
+			uid2 = event.messageReply.senderID;
+		}
+
+		if (!uid2) return message.reply(getLang("noTarget"));
 
 		// Prevent using the command on the owner
 		if (uid2 === OWNER_ID) return message.reply(getLang("cantSlapOwner"));
@@ -45,7 +50,7 @@ module.exports = {
 		const pathSave = `${__dirname}/tmp/${uid1}_${uid2}Batslap.png`;
 		fs.writeFileSync(pathSave, Buffer.from(img));
 
-		const content = args.join(' ').replace(Object.keys(event.mentions)[0], "");
+		const content = args.join(' ').replace(Object.keys(event.mentions)[0] || "", "");
 		message.reply({
 			body: `${(content || "Bópppp 😵‍💫😵")}`,
 			attachment: fs.createReadStream(pathSave)
